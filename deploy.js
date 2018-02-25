3c7b43e297e9682c89a1eaf0b25d40f66d6e5cd3
Michael Meyer <mnm@tomtom>
2018-02-24 20:27:43 -0600

    Start mining when deploying

--- deploy.js
+++ deploy.js
@@ -1,5 +1,9 @@
 var abi_text = {} // FILL ME
-var bin_text = "" // FILL ME
+var bin_text = "" //FILL ME
+
+miner.start() // so the script can have a definite address
+debug.verbosity(2) // 3 is default for info.
+web3.eth.defaultAccount = eth.accounts[0] // set default account when calling the script.
 
 var factory = eth.contract(abi_text)
 var compiled = "0x" + bin_text
